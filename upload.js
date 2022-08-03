61c0fb1d71ddb3bead1208342f248cd797c3eeb9
zhuxiaodong <galaxyxdzhu@gmail.com>
2022-08-03 18:20:05 +0800

    feat: upload 代码优化

--- upload.js
+++ upload.js
@@ -1,7 +1,6 @@
 const multerConfig = require('./multerConfig')
 
 const fileName = 'file'
-const uploadBaseUrl = 'http://127.0.0.1:3000'
 const imgPath = '/img/images'
 
 function upload(req, res) {
